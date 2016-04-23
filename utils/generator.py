7ebd16a8b393b7791d95dfa4dda02005047c2af9
ncteisen <ncteisen@umich.ede>
2016-04-23 11:36:51 -0400

    finished reorg

--- utils/generator.py
+++ utils/generator.py
@@ -5,7 +5,7 @@ import os
 # this will build the html that makes up the library of books
 library_book_elt_html_template = """
 <div class="col-md-3 col-height book" book-title="{title}" uniquename="{uniquename}">
-    <a href="books/{uniquename}/">
+    <a href="{uniquename}/">
         <div class="pic-wrapper">
             <img src="{picture}" class="img-rounded pic" width="175" height="250">
             <br>
@@ -43,14 +43,16 @@ js_text_file_template = open("template/raw_text.js", "r").read()
 
 
 # json objects of all the books
-books = open("books.json", "r").read()
-books = json.loads(books)
+data = open("data.json", "r").read()
+data = json.loads(data)
 
 # sort the books by author, then title
-books = sorted(books["books"], key = book_sort_string)
+books = sorted(data["books"], key = book_sort_string)
+movies = sorted(data["movies"], key = lambda m: m["title"])
 
 # start the html by opening the elements
 book_list_html = "<div class=\"row\"><div class=\"row-height\">"
+movie_list_html = "<div class=\"row\"><div class=\"row-height\">"
 
 col_per_row = 4
 for i, book in enumerate(books):
@@ -107,7 +109,7 @@ for i, book in enumerate(books):
 		uniquename = uniquename,
 		title = book["title"],
 		author = book["author"],
-		picture = "%s/cover.jpg" % (bookdir)
+		picture = "%s/cover.jpg" % (uniquename)
 	)
 
 	# adds row to the bootsrap as needed
@@ -117,16 +119,86 @@ for i, book in enumerate(books):
 	book_list_html += library_book_elt_html
 	book["html"] = library_book_elt_html
 
+for i, movie in enumerate(movies):
+
+	uniquename = movie["uniquename"]
+
+	moviedir = "movies/%s" % uniquename
+
+	# validate all the info
+	if (not os.path.isdir(moviedir)):
+		sys.stderr.write("Error: no directory named %s -- Site won't be generated\n" % moviedir)
+		continue;
+
+	tfile = "%s/script.txt" % (moviedir)
+	if (not os.path.isfile(tfile)):
+		sys.stderr.write("Error: no file %s -- Site won't be generated\n" % tfile)
+		continue;
+
+	pfile = "%s/cover.jpg" % (moviedir)
+	if (not os.path.isfile(pfile)):
+		sys.stderr.write("Error: no file %s -- Site won't be generated\n" % pfile)
+		continue;
+
+	# this will be where we write the generated index file per book
+	html_outfile_name = "%s/index.html" % moviedir
+	html_outfile = open(html_outfile_name, "w")
+
+	# linkified, so the titles are clickable
+	title = linkify(movie["title"])
+	director = linkify(movie["director"])
+
+	# creation of the per book html page
+	html = html_template.format(
+		title = title,
+		author = director
+	)
+
+	html_outfile.write(html)
+
+	# this will be the js file that holds all the text
+	raw_text_file_name = "movies/%s/script.txt" % uniquename
+	raw_text_file = open(raw_text_file_name, "r")
+	raw_text = raw_text_file.read()
+
+	js_text = js_text_file_template.format(text = raw_text)
+
+	js_text_outfile_name = "%s/raw_text.js" % (moviedir)
+	js_text_outfile = open(js_text_outfile_name, "w")
+
+	js_text_outfile.write(js_text)
+
+	# constructing the library html
+	library_movie_elt_html = library_book_elt_html_template.format(
+		uniquename = uniquename,
+		title = movie["title"],
+		author = movie["director"],
+		picture = "%s/cover.jpg" % (uniquename)
+	)
+
+	# adds row to the bootsrap as needed
+	if i and i % col_per_row == 0:
+		movie_list_html += "</div></div><div class=\"row\"><div class=\"row-height\">"
+
+	movie_list_html += library_movie_elt_html
+	movie["html"] = library_movie_elt_html
+
 # make the index for the library
 library = open("template/library.html", "r").read()
-outfile = open("index.html", "w")
+book_outfile = open("books/index.html", "w")
+movie_outfile = open("movies/index.html", "w")
 
 # close elts of the lib html
 book_list_html += "</div></div>"
 
-outfile.write(library.format(book_list=book_list_html))
+book_outfile.write(library.format(book_list=book_list_html, type="books"))
+movie_outfile.write(library.format(book_list=movie_list_html, type="movies"))
 
 # create a file to be used by the index.js file
 books_js_file = open("js/books.js", "w")
 books_js_file.write("book_list = ")
 books_js_file.write(json.dumps(books))
+
+movies_js_file = open("js/movies.js", "w")
+movies_js_file.write("book_list = ")
+movies_js_file.write(json.dumps(movies))
